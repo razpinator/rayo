266658f9535946f68c17b4464587abee4d0761d6
Raza Balbale <ribalbale@gmail.com>
2025-09-09 00:40:58 -0500

    CLI tool setup.

--- cmd/functurec/main.go
+++ cmd/functurec/main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "github.com/spf13/cobra"
+)
+
+var (
+    includePaths []string
+    outputDir string
+    verbose bool
+    emitGo bool
+)
 
 func main() {
-    fmt.Println("functurec - Functure Transpiler CLI\nUsage: functurec [subcommand]\nTry 'functurec --help' for more info.")
+    var rootCmd = &cobra.Command{
+        Use:   "functurec",
+        Short: "Functure Transpiler CLI",
+    }
+
+    rootCmd.PersistentFlags().StringSliceVarP(&includePaths, "include", "I", nil, "Include paths")
+    rootCmd.PersistentFlags().StringVarP(&outputDir, "output", "o", "", "Output directory")
+    rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
+    rootCmd.PersistentFlags().BoolVar(&emitGo, "emit-go", false, "Emit Go code")
+
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "lex",
+        Short: "Lex source file",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Lexing not yet implemented.")
+        },
+    })
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "parse",
+        Short: "Parse source file",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Parsing not yet implemented.")
+        },
+    })
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "check",
+        Short: "Check semantics",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Semantic check not yet implemented.")
+        },
+    })
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "transpile",
+        Short: "Transpile to Go",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Transpilation not yet implemented.")
+        },
+    })
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "run",
+        Short: "Transpile and run",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Run not yet implemented.")
+        },
+    })
+    rootCmd.AddCommand(&cobra.Command{
+        Use: "test",
+        Short: "Run golden tests",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Test not yet implemented.")
+        },
+    })
+
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
